9450caf0009be7f363c124d84e2026ba7a2bd6c5
Hasan Yıldız <hy0782617@gmail.com>
2023-11-01 15:03:34 +0300

    Calculation Application
    An application that allows us to perform different mathematical calculations in the C# programming language.
    
    (C# Programlama dilinde farklı matematiksel hesaplamaları yapmamızı sağlayan bir uygulama)

---
+++ Calculation Application/Fonksiyonlar.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    internal class Fonksiyonlar
+    {
+        //Üçgenin Alanı
+        public double edge1;
+        public double edge2;
+
+        public double Edge1
+        {
+            get
+            {
+                return edge1;
+            }
+
+            set
+            {
+                edge1 = value;
+            }
+
+        }
+
+        public double Edge2
+        {
+            get
+            {
+                return edge2;
+            }
+
+            set
+            {
+                edge2 = value;
+            }
+        }
+
+        public double CalculateArea()
+        {
+            return edge1 * edge2 / 2;
+        }
+
+        //Dikdörtgenin Alanı
+        public double longSide;
+        public double shortEdge;
+        public double LongSide
+        {
+            get
+            {
+                return longSide;
+            }
+            set
+            {
+                longSide = value;
+            }
+        }
+
+        public double ShortEdge
+        {
+            get
+            {
+                return shortEdge;
+            }
+            set
+            {
+                shortEdge = value;
+            }
+        }
+
+        public double CalculateRectangularArea()
+        {
+            return longSide * shortEdge;
+        }
+
+        //Dairenin Alanı
+        //public int area;
+        public double radius;
+        public double pi;
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+
+        public double Pi
+        {
+            get
+            {
+                return pi;
+            }
+            set
+            {
+                pi = value;
+            }
+        }
+
+        public double CircleArea()
+        {
+            return radius * radius * pi;
+        }
+
+
+        //Asal Sayı Kontrol
+        public int number6;
+
+        public int Number6
+        {
+            get
+            {
+                return number6;
+            }
+            set
+            {
+                number6 = value;
+            }
+        }
+
+
+        public int AsalSayiMi()
+        {
+            
+            int sayac = 0;
+            for (int i = 1; i <= number6; i++)
+            {
+                if (number6 % i == 0)
+                {
+                    sayac++;
+                }
+            }
+            
+
+            if(sayac == 2)
+            {
+                Console.WriteLine("Girdiğiniz {0} sayısı Asal Sayıdır", number6);
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz {0} sayısı Asal Sayı Değildir.", number6);
+            }
+            return sayac;
+            
+        }
+        
+
+
+        //Faktoriyel hesaplama
+
+        public int number5;
+        public int faktoryel = 1;
+
+        public int Number5
+        {
+            get
+            {
+                return number5;
+            }
+            set
+            {
+                number5 = value;
+            }
+        }
+
+        public int Faktoryel
+        {
+            get
+            {
+                return faktoryel;
+            }
+            set
+            {
+                faktoryel = value;
+            }
+        }
+
+        public int FaktoryelHesapla()
+        {
+            
+            int faktoriyel = 1;
+            for (int i = 1; i <= number5; i++)
+            {
+                faktoriyel *= i;
+
+            }
+           // Console.WriteLine(sayi5 + "! = " + faktoriyel);
+
+           return faktoriyel;
+            
+        }
+
+        //Negatif mi? Pozitif mi?
+        public double number7;
+
+        public double Number7
+        {
+            get
+            {
+                return number7;
+            }
+            set
+            {
+                number7 = value;
+            }
+        }
+
+        public double PozitifNegatif()
+        {
+            if (number7 > 0)
+            {
+                Console.WriteLine("Girilen sayı POZITIF");
+            }
+            else if (number7 < 0) 
+            { 
+                Console.WriteLine("Girilen sayı NEGATIF"); 
+            }
+            else if (number7 == 0)
+            {
+                Console.WriteLine("Girilen sayı NÖTR");
+            }
+
+            return number7;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Calculation Application/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 - Üçgen Alanı Hesaplama");
+            Console.WriteLine("2 - Dikdörtgen Alanı Hesaplama:");
+            Console.WriteLine("3 - Dairenin Alanı");
+            Console.WriteLine("4 - Asal Sayı mı?");
+            Console.WriteLine("5 - Faktoriyel Hesaplama");
+            Console.WriteLine("6 - Negatif mi Pozitif mi Hesaplama");
+            Console.WriteLine("");
+            Console.Write("Yapmak İstediğiniz İşlem: ");
+            string hesaplama = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("");
+            switch (hesaplama)
+            {
+
+                case "1":
+                    Fonksiyonlar ucgenAlan = new Fonksiyonlar();
+                    Console.WriteLine("Üçgenin Alanını Hesaplama");
+                    Console.WriteLine("");
+                    Console.Write("1. Kenarı giriniz: ");
+                    ucgenAlan.Edge1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("2. Kenarı giriniz: ");
+                    ucgenAlan.Edge2 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("sonuç: {0}", ucgenAlan.CalculateArea());
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("");
+                    Console.ReadKey();
+                    break;
+
+                case "2":
+
+                    //Dikdörtgenin Alanı
+                    Fonksiyonlar dikdörtgenAlan = new Fonksiyonlar();
+                    Console.WriteLine("Dikdörtgenin Alanını Hesaplama");
+                    Console.WriteLine("");
+                    Console.Write("Kısa Kenarı Giriniz: ");
+                    dikdörtgenAlan.shortEdge = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Uzun Kenarı Giriniz: ");
+                    dikdörtgenAlan.longSide = Convert.ToDouble(Console.ReadLine());
+
+                    if (dikdörtgenAlan.shortEdge > dikdörtgenAlan.longSide)
+                    {
+                        Console.WriteLine("Hatalı girdi");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sonuç : {0}", dikdörtgenAlan.CalculateRectangularArea());
+                    }
+                    Console.WriteLine("");
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("");
+                    Console.ReadKey();
+                    break;
+
+                case "3":
+                    //Dairenin Alanı
+                    Fonksiyonlar daireAlan = new Fonksiyonlar();
+                    Console.WriteLine("Dairenin Alanını Hesaplama");
+                    Console.WriteLine("");
+                    Console.Write("Dairenin Yarı Çapını Giriniz: ");
+                    daireAlan.radius = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Pi sayısını giriniz: ");
+                    daireAlan.pi = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Sonuç: {0}", daireAlan.CircleArea());
+                    Console.WriteLine("");
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("");
+                    Console.ReadKey();
+                    break;
+
+                case "4":
+                    //Asal sayı mı?
+                    Fonksiyonlar asalSayi = new Fonksiyonlar();
+                    Console.WriteLine("Asal Sayı mı?");
+                    Console.WriteLine("");
+                    Console.Write("Sayınızı Giriniz: ");
+                    asalSayi.number6 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(asalSayi.AsalSayiMi());
+                    Console.WriteLine("");
+                    Console.WriteLine("---------------------------------");
+
+                    Console.WriteLine("");
+                    Console.ReadKey();
+                    break;
+
+                case "5":
+                    //Faktoriyel Hesaplama
+                    Fonksiyonlar faktoriyel = new Fonksiyonlar();
+                    Console.WriteLine("Faktoriyel Hesaplama");
+                    Console.WriteLine("");
+                    Console.Write("Sayınızı Giriniz: ");
+                    faktoriyel.number7 = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("sonuç: {0}", faktoriyel.FaktoryelHesapla());
+                    Console.WriteLine("");
+                    Console.WriteLine("---------------------------------");
+
+                    Console.WriteLine("");
+                    Console.ReadKey();
+                    break;
+
+                case "6":
+                    //Negatif mi? Pozitif mi?
+
+                    Fonksiyonlar pozitifNegatif = new Fonksiyonlar();
+                    Console.WriteLine("Sayınız Pozitif Mi? Negatif Mi?");
+                    Console.WriteLine("");
+                    Console.Write("Sayınızı Giriniz: ");
+                    pozitifNegatif.number7 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("");
+                    Console.WriteLine("Sayınız {0}", pozitifNegatif.PozitifNegatif());
+                    Console.WriteLine("");
+                    Console.WriteLine("---------------------------------");
+
+                    Console.WriteLine("");
+                    Console.ReadKey();
+                    break;
+                default: Console.WriteLine("HATALI İŞLEM YAPTINIZ");
+                    break;
+            }
+        }
+
+
+
+
+
+
+
+    }
+    }
